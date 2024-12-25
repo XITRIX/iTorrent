9b1ecabb97397aeed4e28a9ff2fa43cc01d849d9
Daniil Vinogradov <xitrix@bk.ru>
2024-12-25 17:14:24 +0100

    Minor concurrency fix

--- iTorrent/Core/SceneDelegate/SceneDelegate+AVPlayer.swift
+++ iTorrent/Core/SceneDelegate/SceneDelegate+AVPlayer.swift
@@ -21,31 +21,19 @@ extension SceneDelegate {
 extension SceneDelegate {
     class AVPlayerDelegates: DelegateObject<SceneDelegate>, AVPlayerViewControllerDelegate {
         func playerViewControllerRestoreUserInterfaceForFullScreenExit(_ playerViewController: AVPlayerViewController) async -> Bool {
-            await withCheckedContinuation { continuation in
-                Task { @MainActor in
-                    guard let vc = parent.window?.rootViewController?.topPresented else {
-                        return continuation.resume(returning: false)
-                    }
+            guard let vc = await parent.window?.rootViewController?.topPresented
+            else { return false }
 
-                    vc.present(playerViewController, animated: true) {
-                        continuation.resume(returning: true)
-                    }
-                }
-            }
+            await vc.present(playerViewController, animated: true)
+            return true
         }
 
         func playerViewControllerRestoreUserInterfaceForPictureInPictureStop(_ playerViewController: AVPlayerViewController) async -> Bool {
-            await withCheckedContinuation { continuation in
-                Task { @MainActor in
-                    guard let vc = parent.window?.rootViewController?.topPresented else {
-                        return continuation.resume(returning: false)
-                    }
+            guard let vc = await parent.window?.rootViewController?.topPresented
+            else { return false }
 
-                    vc.present(playerViewController, animated: true) {
-                        continuation.resume(returning: true)
-                    }
-                }
-            }
+            await vc.present(playerViewController, animated: true)
+            return true
         }
     }
 
