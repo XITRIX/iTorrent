e14572e8f25f92fbf2e831984c265586c4961313
XITRIX <xitrix@bk.ru>
2026-03-17 00:47:43 +0100

    Multiline textfield alert iOS 26 style

--- iTorrent/Components/Views/EditTextView/EditTextView.swift
+++ iTorrent/Components/Views/EditTextView/EditTextView.swift
@@ -11,9 +11,6 @@ import UIKit
 
 // EditTextView - UI Control
 class EditTextView: UITextView {
-    // Set margins using UIEdgeInsets
-    let defaultMargins = UIEdgeInsets(top: 4, left: 12, bottom: 12, right: 12)
-    
     // Custom placeholder text
     var placeholder: String? {
         didSet { placeholderLabel.text = placeholder }
@@ -50,22 +47,41 @@ class EditTextView: UITextView {
     /* Helper methods */
     private func performCustomizations() {
         let textView = self
-        
-        textView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+
         textView.font = UIFont.systemFont(ofSize: UIFont.systemFontSize * 1.0)
-        textView.contentInset.left =    2 // For left padding
-        textView.contentInset.right =   2 // For right padding
-        textView.contentInset.top =     2 // For top padding
-        textView.contentInset.bottom =  2 // For bottom padding
 
         textView.layer.isOpaque = false
-        textView.layer.cornerRadius = 8
-        textView.borderColor = UIColor.lightGray.withAlphaComponent(0.5)
         textView.layer.backgroundColor = nil
         textView.returnKeyType = .default
         textView.autocapitalizationType = .none
        
         textView.translatesAutoresizingMaskIntoConstraints = false
+
+        textView.textContainerInset = .zero
+        if #available(iOS 26, *) {
+            textView.layer.cornerRadius = 25
+
+            textView.contentInset.top =     16 // For top padding
+            textView.contentInset.bottom =  16 // For bottom padding
+
+            textView.textContainerInset.left =    12 // For left padding
+            textView.textContainerInset.right =   12 // For right padding
+            textView.textContainerInset.top =     0 // For top padding
+            textView.textContainerInset.bottom =  0 // For bottom padding
+        } else {
+            textView.layer.cornerRadius = 8
+
+            textView.contentInset.top =     8 // For top padding
+            textView.contentInset.bottom =  8 // For bottom padding
+
+            textView.textContainerInset.left =    2 // For left padding
+            textView.textContainerInset.right =   2 // For right padding
+            textView.textContainerInset.top =     0 // For top padding
+            textView.textContainerInset.bottom =  0 // For bottom padding
+
+            textView.borderColor = UIColor.lightGray.withAlphaComponent(0.5)
+        }
+        
         setupPlaceholderLabel()
     }
     
@@ -78,10 +94,10 @@ class EditTextView: UITextView {
         placeholderLabel.translatesAutoresizingMaskIntoConstraints = false
         // Set constraints for the placeholder label
         NSLayoutConstraint.activate([
-            placeholderLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: contentInset.left+4),
-            placeholderLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -(contentInset.right+4)),
-            placeholderLabel.topAnchor.constraint(equalTo: topAnchor, constant: contentInset.top+6),
-            placeholderLabel.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -(contentInset.bottom+6))
+            placeholderLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: textContainerInset.left+4),
+            placeholderLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -(textContainerInset.right+4)),
+            placeholderLabel.topAnchor.constraint(equalTo: topAnchor, constant: textContainerInset.top),
+            placeholderLabel.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -(textContainerInset.bottom))
         ])
         
         // Hide the placeholder label when there is text
@@ -90,30 +106,5 @@ class EditTextView: UITextView {
     
     @objc private func textDidChange() {
         placeholderLabel.isHidden = !text.isEmpty
-    }
-    
-    public func setDefaultMargins() {
-        setMargins(by: defaultMargins)
-    }
-    
-    public func setMargins(by: UIEdgeInsets) {
-        let margins = by
-        if let containerView = superview {
-            frame = containerView.frame
-            // Add constraints to the UITextView within the parent view
-            NSLayoutConstraint.activate([
-                leadingAnchor.constraint(equalTo: containerView.leadingAnchor, constant: margins.left),
-                trailingAnchor.constraint(equalTo: containerView.trailingAnchor, constant: -margins.right),
-                topAnchor.constraint(equalTo: containerView.topAnchor, constant: margins.top),
-                bottomAnchor.constraint(equalTo: containerView.bottomAnchor, constant: -margins.bottom)
-            ])
-        }
-    }
-    
-    // callback for superview changes
-    override func didMoveToSuperview() {
-        // attach superView's frame reference into this view
-        // and set margins if superView is present
-        setDefaultMargins()
     }
 }
--- iTorrent/Components/Views/EditTextView/EditTextViewController.swift
+++ iTorrent/Components/Views/EditTextView/EditTextViewController.swift
@@ -11,7 +11,13 @@ import UIKit
 
 class EditTextViewController: UIViewController, UITextViewDelegate {
     private let editTextView: EditTextView
-    private let SINGLE_LINE_HEIGHT: Double = 54.0
+    private var SINGLE_LINE_HEIGHT: Double {
+        if #available(iOS 26.0, *) {
+            54
+        } else {
+            36
+        }
+    }
 
     convenience init() {
         self.init(editTextView: EditTextView())
@@ -30,14 +36,32 @@ class EditTextViewController: UIViewController, UITextViewDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         // add the UITextView
+        editTextView.translatesAutoresizingMaskIntoConstraints = false
         view.addSubview(editTextView)
+
+        view.preservesSuperviewLayoutMargins = true
+
+        NSLayoutConstraint.activate([
+            view.layoutMarginsGuide.leadingAnchor.constraint(equalTo: editTextView.leadingAnchor),
+            view.topAnchor.constraint(equalTo: editTextView.topAnchor, constant: -2),
+            view.layoutMarginsGuide.trailingAnchor.constraint(equalTo: editTextView.trailingAnchor),
+            view.bottomAnchor.constraint(equalTo: editTextView.bottomAnchor, constant: 2),
+        ])
+
         // register callback for textView changes
         editTextView.delegate = self
         // Create a height constraint for the view controller's view
         viewControllerHeightConstraint = view.heightAnchor.constraint(equalToConstant: SINGLE_LINE_HEIGHT)
         viewControllerHeightConstraint.isActive = true
     }
-    
+
+    override func viewIsAppearing(_ animated: Bool) {
+        super.viewIsAppearing(animated)
+
+        self.view.setNeedsLayout()
+        self.view.layoutIfNeeded()
+    }
+
     var viewControllerHeightConstraint: NSLayoutConstraint!
     
     func textViewDidChange(_ textView: UITextView) {
